7d198317ffeef593c49100d99a0f45a63640e947
ajlopez <webmaster@ajlopez.com>
2017-09-23 11:22:03 -0300

    Encode string value

--- test/value.js
+++ test/value.js
@@ -10,3 +10,12 @@ exports['encode integer'] = function (test) {
     test.equal(result, '000000000000000000000000000000000000000000000000000000000000002a');
 };
 
+exports['encode string'] = function (test) {
+    var result = simpleabi.encodeValue("hello");
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 64 * 2);
+    test.equal(result, '0000000000000000000000000000000000000000000000000000000000000005' + Buffer.from('hello').toString('hex') + '000000000000000000000000000000000000000000000000000000');
+};
+
