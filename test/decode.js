1100d31160e3850746c59a59a46b949293cc81e9
ajlopez <webmaster@ajlopez.com>
2017-12-25 18:48:51 -0300

    Decode two simple values

--- test/decode.js
+++ test/decode.js
@@ -9,3 +9,13 @@ exports['decode simple value'] = function (test) {
 	test.equal(result, 1);
 }
 
+exports['decode two simple values'] = function (test) {
+	var encoded = '0x' + simpleabi.encodeValues([1,2]);
+	
+	var result = simpleabi.decodeValues(encoded);
+	
+	test.equal(result.length, 2);
+	test.equal(result[0], 1);
+	test.equal(result[1], 2);
+}
+
