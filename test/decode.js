7694c31a6399cc35f1e5e5209c0c15a73249b750
ajlopez <webmaster@ajlopez.com>
2017-12-25 19:06:32 -0300

    Decode values with types

--- test/decode.js
+++ test/decode.js
@@ -10,12 +10,24 @@ exports['decode simple value'] = function (test) {
 }
 
 exports['decode two simple values'] = function (test) {
-	var encoded = '0x' + simpleabi.encodeValues([1,2]);
+	var encoded = '0x' + simpleabi.encodeValues([ 1, 2 ]);
 	
 	var result = simpleabi.decodeValues(encoded);
 	
 	test.equal(result.length, 2);
 	test.equal(result[0], 1);
 	test.equal(result[1], 2);
+}
+
+exports['decode values with types'] = function (test) {
+	var encoded = '0x' + simpleabi.encodeValues([ 1, "hello", 2, 42 ]);
+	
+	var result = simpleabi.decodeValues(encoded, [ "uint256", "string", "uint256", "uint256" ]);
+	
+	test.equal(result.length, 4);
+	test.equal(result[0], 1);
+	test.equal(result[1], "hello");
+	test.equal(result[2], 2);
+	test.equal(result[3], 42);
 }
 
