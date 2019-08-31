01fe0e2995c21f349c92d9d06b1fea0981aec4ec
ajlopez <webmaster@ajlopez.com>
2019-08-31 17:53:09 -0300

    Encode call with string argument

--- test/call.js
+++ test/call.js
@@ -46,3 +46,16 @@ exports['encode call with hexadecimal string value'] = function (test) {
 		+ '0000000000000000000000000000000000000000000000000000000001020304');
 };
 
+exports['encode call with numeric string value as string'] = function (test) {
+    var result = simpleabi.encodeCall('setMessage(string)', [ "42" ]);
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 8 + 64 * 3);
+    test.equal(result, 
+        keccak256('setMessage(string)').substring(0, 8)
+		+ '0000000000000000000000000000000000000000000000000000000000000020'
+		+ '0000000000000000000000000000000000000000000000000000000000000002'
+		+ '3432000000000000000000000000000000000000000000000000000000000000');
+};
+
