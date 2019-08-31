c8287a91b44c1bab048498afa94eb1f67285b9c1
ajlopez <webmaster@ajlopez.com>
2019-08-31 17:31:31 -0300

    Encode call with integer values as arguments

--- test/call.js
+++ test/call.js
@@ -11,3 +11,13 @@ exports['encode function call without arguments'] = function (test) {
     test.equal(result, keccak256('increment()').substring(0, 8));
 };
 
+exports['encode call with integer values'] = function (test) {
+    var result = simpleabi.encodeCall('add(uint256,uint256,uint256)', [ 1, 2, 3 ]);
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 8 + 64 * 3);
+    test.equal(result, 
+        keccak256('add(uint256,uint256,uint256)').substring(0, 8)
+        + '000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003');
+};
