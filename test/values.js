bebd0d476f65fa3378bcbca221939280b8922ad6
ajlopez <webmaster@ajlopez.com>
2019-08-31 17:12:18 -0300

    Encode numeric string as string

--- test/values.js
+++ test/values.js
@@ -22,6 +22,28 @@ exports['encode string value'] = function (test) {
 		+ simpleabi.stringToBuffer('hello').toString('hex') + '000000000000000000000000000000000000000000000000000000');
 };
 
+exports['encode hexadecimal string value'] = function (test) {
+    var result = simpleabi.encodeValues([ "0x01020304" ]);
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 64);
+    test.equal(result, 
+		'0000000000000000000000000000000000000000000000000000000001020304');
+};
+
+exports['encode numeric string value as string'] = function (test) {
+    var result = simpleabi.encodeValues([ "42" ], [ 'string' ]);
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 64 * 3);
+    test.equal(result, 
+		  '0000000000000000000000000000000000000000000000000000000000000020'
+		+ '0000000000000000000000000000000000000000000000000000000000000002'
+		+ '3432000000000000000000000000000000000000000000000000000000000000');
+};
+
 exports['encode hexadecimal string as bytes'] = function (test) {
     var result = simpleabi.encodeValues([ '0x123456' ], [ 'bytes' ]);
     
