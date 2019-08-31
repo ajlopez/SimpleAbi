478b2c6af168e6ef9e76799fe1555f43ff3b6d33
ajlopez <webmaster@ajlopez.com>
2019-08-31 17:45:31 -0300

    Additional tests, encoding call with numeric string and address

--- test/call.js
+++ test/call.js
@@ -21,3 +21,28 @@ exports['encode call with integer values'] = function (test) {
         keccak256('add(uint256,uint256,uint256)').substring(0, 8)
         + '000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000003');
 };
+
+exports['encode call with string value'] = function (test) {
+    var result = simpleabi.encodeCall('setMessage(string)', [ "hello" ]);
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 8 + 64 * 3);
+    test.equal(result, 
+        keccak256('setMessage(string)').substring(0, 8)
+		+ '0000000000000000000000000000000000000000000000000000000000000020'
+		+ '0000000000000000000000000000000000000000000000000000000000000005'
+		+ simpleabi.stringToBuffer('hello').toString('hex') + '000000000000000000000000000000000000000000000000000000');
+};
+
+exports['encode call with hexadecimal string value'] = function (test) {
+    var result = simpleabi.encodeCall('setManager(address)', [ "0x01020304" ]);
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 8 + 64);
+    test.equal(result, 
+        keccak256('setManager(address)').substring(0, 8)
+		+ '0000000000000000000000000000000000000000000000000000000001020304');
+};
+
