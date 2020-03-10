c2091356a88ffc9b1ef69c48968f4259e164325e
ajlopez <webmaster@ajlopez.com>
2020-03-10 05:19:04 -0300

    Encode function signature

--- test/call.js
+++ test/call.js
@@ -2,6 +2,15 @@
 const simpleabi = require('..');
 const keccak256 = require('../lib/sha3').keccak_256;
 
+exports['encode function signature'] = function (test) {
+    const result = simpleabi.encodeFunction('add(uint256)');
+    
+    test.ok(result);
+    test.equal(typeof result, 'string');
+    test.equal(result.length, 8);
+    test.equal(result, keccak256('add(uint256)').substring(0, 8));
+};
+
 exports['encode function call without arguments'] = function (test) {
     const result = simpleabi.encodeCall('increment()');
     
