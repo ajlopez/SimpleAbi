dfcd3523e48f9bd6d8b600e25654899a05cfc4b1
ajlopez <webmaster@ajlopez.com>
2020-07-26 11:45:32 -0300

    Safeint test minor refactor

--- test/safeint.js
+++ test/safeint.js
@@ -1,21 +1,21 @@
 
-var simpleabi = require('..');
+const simpleabi = require('..');
 
 exports['one as safe integer'] = function (test) {
-	var result = simpleabi.asSafeInteger('0x' + simpleabi.encodeValue(1));
+	const result = simpleabi.asSafeInteger('0x' + simpleabi.encodeValue(1));
 	
 	test.strictEqual(result, 1);
 }
 
 exports['one million as safe integer'] = function (test) {
-	var result = simpleabi.asSafeInteger('0x' + simpleabi.encodeValue(1000000));
+	const result = simpleabi.asSafeInteger('0x' + simpleabi.encodeValue(1000000));
 	
 	test.strictEqual(result, 1000000);
 }
 
 exports['big number is not a safe integer'] = function (test) {
-	var number = '0x1000000000000000000000';
-	var result = simpleabi.asSafeInteger(number);
+	const number = '0x1000000000000000000000';
+	const result = simpleabi.asSafeInteger(number);
 	
 	test.strictEqual(result, number);
 }
